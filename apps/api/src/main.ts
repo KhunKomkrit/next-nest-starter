c27b11e25ccf7723db41cb7b3063785523444874
BankFullStack <komkrit.nookhampan@gmail.com>
2024-10-03 18:51:16 +0700

    add api with nest

---
+++ apps/api/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '.env.local' });
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on:  http://localhost:${port}`);
+}
+bootstrap();
